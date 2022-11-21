02bd40912d91aba727f54ac56fdc73d6f4529fdd
Marina Dimova <117082057+MaarinaD@users.noreply.github.com>
2022-11-21 16:05:07 +0200

    Add files via upload
    800/800 ✔️

---
+++ 07.Loops/02.js
@@ -0,0 +1,9 @@
+function reverseNumbers(input) {
+    let number = Number(input[0]);
+
+    for(let i = number; i > 0; i--) {
+        console.log(i);
+    }
+}
+
+reverseNumbers(["2"]);
---
+++ 07.Loops/04.js
@@ -0,0 +1,12 @@
+function evenNums(input) {
+    let n = Number(input[0]);
+
+    for(i = 0; i <= n; i++) {
+        if(i % 2 === 0) {
+            let number = Math.pow(2, i);
+            console.log(number);
+        }
+    }
+}
+
+evenNums(['4'])
